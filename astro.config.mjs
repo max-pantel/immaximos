61a4bf0822cd93b4201c8c661d4f062fa4f308b2
Fyodor Dostoevsky <pantel.maxim@gmail.com>
2025-10-20 02:07:15 +0300

    Add Github Pages deployment workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,8 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({site: "https://immaximos.com', base: '/', outDir: './dist',});
+export default defineConfig({
+	site: "https://immaximos.com", 
+	base: "/", 
+	outDir: "./dist"
+	});
